b099fcdb7854326505802e560d7e9da93e8eed51
Jamie Goodnight <goodnightlambda@gmail.com>
2019-01-28 15:16:01 -0500

    Getting started

--- js/index.js
+++ js/index.js
@@ -40,3 +40,20 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const logo = document.getElementById("logo-img");
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+const navItems=document.querySelectorAll('nav a');
+navItems[0].textContent=siteContent['nav']['nav-item-1'];
+navItems[1].textContent=siteContent['nav']['nav-item-2'];
+navItems[2].textContent=siteContent['nav']['nav-item-3'];
+navItems[3].textContent=siteContent['nav']['nav-item-4'];
+navItems[4].textContent=siteContent['nav']['nav-item-5'];
+navItems[5].textContent=siteContent['nav']['nav-item-6'];
+
+const h1Content=document.querySelector('.cta h1');
+h1Content.textContent="Dom is Awesome!"
+
+const btnContent=document.querySelector('.cta button');
+btnContent.textContent="Get Started";
